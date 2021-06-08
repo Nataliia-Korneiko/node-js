bf929f7c2a1811b4f38967860ca0a8462bc60cb5
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2021-06-09 00:47:09 +0300

    added lesson-04

---
+++ lesson-04/src/services/cats.js
@@ -0,0 +1,37 @@
+const { CatsRepository } = require('../repository');
+
+class CatsService {
+  constructor() {
+    this.repositories = {
+      cats: new CatsRepository(),
+    };
+  }
+
+  getAll() {
+    const data = this.repositories.cats.getAll();
+    return data;
+  }
+
+  getById({ id }) {
+    const data = this.repositories.cats.getById(id);
+    return data;
+  }
+
+  create(body) {
+    const data = this.repositories.cats.create(body);
+    return data;
+  }
+
+  update({ id }, body) {
+    console.log('CatsRepository -> update -> id:', id);
+    const data = this.repositories.cats.update(id, body);
+    return data;
+  }
+
+  remove({ id }) {
+    const data = this.repositories.cats.remove(id);
+    return data;
+  }
+}
+
+module.exports = CatsService;
---
+++ lesson-04/src/validation/cats.js
@@ -0,0 +1,45 @@
+const Joi = require('joi');
+const { HttpCode } = require('../helpers/constants');
+
+const schemaCreateCat = Joi.object({
+  name: Joi.string().alphanum().min(2).max(30).required(),
+  age: Joi.number().integer().min(1).max(45).required(),
+  isVaccinated: Joi.boolean().optional(),
+});
+
+const schemaUpdateCat = Joi.object({
+  name: Joi.string().alphanum().min(2).max(30).optional(),
+  age: Joi.number().integer().min(1).max(45).optional(),
+  isVaccinated: Joi.boolean().optional(),
+});
+
+const schemaUpdateStatusCat = Joi.object({
+  isVaccinated: Joi.boolean().required(),
+});
+
+const validate = (schema, body, next) => {
+  const { error } = schema.validate(body);
+
+  if (error) {
+    const [{ message }] = error.details; // деструктуризация массива
+
+    return next({
+      status: HttpCode.BAD_REQUEST,
+      message: `Field: ${message.replace(/"/g, '')}`,
+      data: 'Bad Request',
+    });
+  }
+  next();
+};
+
+module.exports.validateCreateCat = (req, _res, next) => {
+  return validate(schemaCreateCat, req.body, next);
+};
+
+module.exports.validateUpdateCat = (req, _res, next) => {
+  return validate(schemaUpdateCat, req.body, next);
+};
+
+module.exports.validateUpdateStatusCat = (req, _res, next) => {
+  return validate(schemaUpdateStatusCat, req.body, next);
+};
